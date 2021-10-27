fae374d42340d67bb4cd17586fe736d9d930aee2
Yuyeong <403467@naver.com>
2021-10-27 13:54:17 +0900

    211027/4

---
+++ backend/first/src/com/koreait/server/WorldServlet.java
@@ -0,0 +1,21 @@
+package com.koreait.server;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/WorldServlet")
+public class WorldServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+
+    }
+}
